fd79a620d0cedb4534303d2dff33076a0d99570f
Alexander Mills <alex@oresoftware.com>
2018-10-09 10:20:30 -0700

    ores/auto-commit => 'set'

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,60 @@
 'use strict';
 
-export const foo = 'bar';
+
+
+import Timer = NodeJS.Timer;
 
 export const r2gSmokeTest = function () {
   // r2g command line app uses this exported function
   return true;
 };
 
-/*
 
- bad library design:
- module.exports = 'foo';
- export = 'foo';
+import {RequestHandler, Request} from "express";
 
- good library design:
- exports.x = 'foo'
- export const x = 'foo'
- export default = 'foo';
+export interface TimeOutMiddlewareOpts {
+  ms: number,
+  forward: boolean
+}
 
-*/
+interface TimeoutInfo {
+  millisCreatedAt: number,
+  timeout: Timer
+}
 
+const container = {
+   requestToInfo: new Map<Request, TimeoutInfo>()
+};
+
+export const timeoutmw = (v: TimeOutMiddlewareOpts) : RequestHandler => {
+
+
+  return (req, res, next) => {
+
+    const info = container.requestToInfo.get(req);
+
+    if(!info){
+
+      const to = setTimeout(() => {
+         res.json({error: new Error('time out.')});
+      }, v.ms);
+
+      container.requestToInfo.set(req, {timeout: to, millisCreatedAt: Date.now()});
+      return next();
+    }
+
+    const now = Date.now();
+
+
+
+  };
+
+
+};
+
+
+
+
+export default timeoutmw;
 
 
